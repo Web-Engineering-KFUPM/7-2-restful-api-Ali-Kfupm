100fdc0887594f10f6092d6bbd13a896433c1e5c
Ali-Kfupm <s202322750@kfupm.edu.sa>
2026-04-15 10:15:22 +0300

    Complete TODO 4

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -44,6 +44,16 @@ app.post("/api/songs", async (req, res) => {
   }
 });
 
+app.get("/api/songs", async (req, res) => {
+  const rows = await Song.find().sort({ createdAt: -1 });
+  res.json(rows);
+});
+
+app.get("/api/songs/:id", async (req, res) => {
+  const s = await Song.findById(req.params.id);
+  if (!s) return res.status(404).json({ message: "Song not found" });
+  res.json(s);
+});
 // api/songs (Insert song)
 
 // /api/songs/:id (Update song)
